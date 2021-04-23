f24137521d72b14be2cbaf3e92e905329052474b
TRAN VINH THUY <mr.thuytran@gmail.com>
2021-04-23 09:23:25 +0700

    initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { fromEventPattern, Observable, Subject } from 'rxjs';
+import { debounceTime, map, tap } from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  releases = [
+    {
+      prjRelease: {
+        id: 1,
+        name: 'ABC',
+        version: 'v1',
+        revision: 0
+      },
+      predecessor: {
+        id: 1.1,
+        name: 'ABC',
+        version: 'v0',
+        revision: 2
+      }
+    },
+    {
+      prjRelease: {
+        id: 2,
+        name: 'XYZ',
+        version: 'v121',
+        revision: 4
+      },
+      predecessor: {
+        id: 2.1,
+        name: 'XYZ',
+        version: 'v120',
+        revision: 0
+      }
+    },
+    {
+      prjRelease: {
+        id: 3,
+        name: 'FGH',
+        version: 'v788',
+        revision: 2
+      },
+      predecessor: {
+        id: 3.1,
+        name: 'FGH',
+        version: 'v787',
+        revision: 4
+      }
+    }
+  ]
+
+  myform:FormGroup;
+  inputChanged$ = new Subject();
+  filteredOptions: Observable<any>;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit() {
+    this.myform = this.fb.group({
+      configuredReleases: this.fb.array(this.initReleaseControls())
+    })
+
+    this.filteredOptions = this.inputChangedObservable
+      .pipe(
+        map(({value, control}) => {
+          console.log(value);
+
+          return value ? this._filter(value) : this.releases.map(r=> r.prjRelease)
+        }),
+        tap(console.log),
+        // map(opt => opt.map((r)=>r.prjRelease)),
+        // tap(console.log),
+      )
+  }
+
+  initReleaseControls() {
+    return this.releases.map((el)=> {
+      return this.fb.group({
+        prjRelease: new FormControl(el.prjRelease),
+        predecessor: new FormControl(el.predecessor)
+      })
+    })
+  }
+
   title = 'rx-form-nested-array';
+
+  get inputChangedObservable() {
+    return this.inputChanged$.asObservable()
+  }
+
+  get releaseControls() {
+    return this.myform.get('configuredReleases') as FormArray;
+  }
+
+  displayFn(release) {
+    return release && release.name ? `${release.name} ${release.version}/${release.revision}` : ''
+  }
+
+  preInputChanged(value, control) {
+    this.inputChanged$.next({value: value.target.value, control})
+  }
+
+  private _filter(name: string): any[] {
+    const filterValue = name.toLowerCase();
+
+    return this.releases
+      .map(r => r.prjRelease)
+      .filter(option => {
+        const name = `${option.name} ${option.version}/${option.revision}`;
+        return name.toLowerCase().indexOf(filterValue) > -1
+      });
+  }
+
+  save() {
+    console.log(this.myform.getRawValue().configuredReleases)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    NoopAnimationsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
